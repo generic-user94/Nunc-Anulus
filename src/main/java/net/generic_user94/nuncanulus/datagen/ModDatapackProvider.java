d83da3dc3418e23084fbb56896b1a2a48029714c
Whitman <whitmandouglasphillips@gmail.com>
2026-03-25 21:25:55 -0500

    idk

--- src/main/java/net/generic_user94/nuncanulus/datagen/ModDatapackProvider.java
+++ src/main/java/net/generic_user94/nuncanulus/datagen/ModDatapackProvider.java
@@ -1,14 +1,25 @@
 package net.generic_user94.nuncanulus.datagen;
 
 import net.generic_user94.nuncanulus.NuncAnulus;
+import net.generic_user94.nuncanulus.worldgen.ModBiomeModifiers;
+import net.generic_user94.nuncanulus.worldgen.ModConfiguredFeatures;
+import net.generic_user94.nuncanulus.worldgen.ModPlacedFeatures;
 import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
+
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
 
     public ModDatapackProvider(PackOutput output, CompletableFuture<RegistrySetBuilder.PatchedRegistries> registries) {
         super(output, registries, Set.of(NuncAnulus.MOD_ID));
